91e9856426bbc145355019b1209386d983714384
Shishir004 <shishirtiwari138@gmail.com>
2025-02-05 08:42:08 +0530

    ready for production

--- frontend/01project/src/store/useAuthStore.js
+++ frontend/01project/src/store/useAuthStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import { toast } from "react-toastify";
 import {io} from 'socket.io-client';
-const BASE_URL='http://localhost:3000';
+const BASE_URL=import.meta.env.MODE==="devlopment"?'http://localhost:3000':'/';
 export const useAuthStore = create((set,get) => ({
     authUser: null,
     isSignInUp: false,
