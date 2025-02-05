9c3971fe266a1e462b66929bb01741086ff1882e
Shishir004 <shishirtiwari138@gmail.com>
2025-02-05 08:43:07 +0530

    backend is ready for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const authRoutes = require('../routes/auth.route');
 const messageRoutes=require('../routes/message.route');
 const cors=require('cors');
+const path=require('path');
 const dbconnection =require('../lib/db');
 const cookieParser=require('cookie-parser');
 require('dotenv').config();
 dbconnection();
 const port=process.env.PORT
+const __dirname=path.resolve();
 const {app,server}=require('../lib/socket');
 
 app.use(express.json({ limit: '10mb' }));
@@ -19,6 +21,14 @@ app.use(cors({
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes); 
+
+if(process.env.NODE_ENV==="production")
+{
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+}
 
 server.listen(port,() => {
     console.log(`Server is running on port ${port}`);
